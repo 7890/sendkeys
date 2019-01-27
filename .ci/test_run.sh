b95d6369bba4d5ef7f80e2ffae88e36f00cd9506
Thomas Brand <tom@trellis.ch>
2019-01-27 15:47:11 +0100

    travis

--- .ci/test_run.sh
+++ .ci/test_run.sh
@@ -7,13 +7,13 @@ echo "========================================================================="
 #fi
 sk --help
 
-oscdump 7778 2>&1 > /tmp/out.txt &
-pid=$!
+#oscdump 7778 2>&1 > /tmp/out.txt &
+#pid=$!
 sleep 1
 echo "abc d"|sk
 sleep 1
-kill -9 $pid
-cat /tmp/out.txt
+#kill -9 $pid
+#cat /tmp/out.txt
 echo "========================================================================="
 echo "`date`"
 echo "done"
